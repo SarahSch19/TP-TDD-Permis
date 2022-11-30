48c287e7fa980a67a8940a9a483a712a576450de
Sarah <sschlegel@myges.fr>
2022-11-30 11:40:01 +0100

    create driving licence service

---
+++ src/main/java/fr/esgi/cleancode/service/CreateDrivingLicenceService.java
@@ -0,0 +1,32 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
+import fr.esgi.cleancode.model.DrivingLicence;
+import fr.esgi.cleancode.utils.SocialSecurityNumberValidator;
+
+public class CreateDrivingLicenceService {
+
+    private final InMemoryDatabase db = InMemoryDatabase.getInstance();
+
+    private static CreateDrivingLicenceService INSTANCE;
+
+    private CreateDrivingLicenceService(){}
+
+    public synchronized static CreateDrivingLicenceService getInstance() {
+        if (INSTANCE == null) {
+            return new CreateDrivingLicenceService();
+        }
+        return INSTANCE;
+    }
+
+    public DrivingLicence save(DrivingLicence drivingLicence){
+        final var socialSecurityNumberIsValid = SocialSecurityNumberValidator.validateSocialSecurityNumber(
+                drivingLicence.getDriverSocialSecurityNumber()
+        );
+        if(!socialSecurityNumberIsValid)
+            throw new InvalidDriverSocialSecurityNumberException("Invalid social security number");
+
+        return db.save(drivingLicence.getId(), drivingLicence);
+    }
+}
