b7207694088f78c1324613a678395deeb649f113
Sarah <sschlegel@myges.fr>
2022-11-30 12:08:55 +0100

    find driving licence test

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,18 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -15,14 +22,33 @@ class DrivingLicenceFinderServiceTest {
 
     @Mock
     private InMemoryDatabase database;
+    private DrivingLicence validDrivingLicence ;
+    private final UUID drivingLicenceId = DrivingLicenceIdGenerationService.generateNewDrivingLicenceId();
 
     @Test
-    void should_find() {
+    void shouldFindDrivingLicenceInDatabase() {
+        final var validSocialSecurityNumber = "123456789012345" ;
+        validDrivingLicence = DrivingLicence.builder()
+                .id(drivingLicenceId)
+                .driverSocialSecurityNumber(validSocialSecurityNumber)
+                .build();
+        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(validDrivingLicence));
 
+        final var actual = service.findById(drivingLicenceId);
+
+        Assertions.assertSame(Optional.ofNullable(validDrivingLicence), actual);
+        verify(database).findById(drivingLicenceId);
+        verifyNoMoreInteractions(database);
     }
 
     @Test
-    void should_not_find() {
+    void shouldNotFindDrivingLicenceInDatabase() {
+        when(database.findById(drivingLicenceId)).thenReturn(Optional.empty());
 
+        final var actual = service.findById(drivingLicenceId);
+
+        Assertions.assertNull(actual);
+        verify(database).findById(drivingLicenceId);
+        verifyNoMoreInteractions(database);
     }
 }
