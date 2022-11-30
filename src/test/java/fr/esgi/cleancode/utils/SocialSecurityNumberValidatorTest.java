758f553848c3a8fd73fd370086a602d127792fb6
Sarah <sschlegel@myges.fr>
2022-11-30 10:58:11 +0100

    social security number validator util tests

---
+++ src/test/java/fr/esgi/cleancode/utils/SocialSecurityNumberValidatorTest.java
@@ -0,0 +1,36 @@
+package fr.esgi.cleancode.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SocialSecurityNumberValidatorTest {
+    private final String socialSecurityNumberShort = "12345678901234" ;
+    private final String socialSecurityNumberLong = "1234567890123456" ;
+    private final String socialSecurityNumberWithLetter = "1234567abc12345" ;
+    private final String validSocialSecurityNumber = "123456789012345" ;
+
+
+    @Test
+    public void shouldNotValidateSocialSecurityNumberTooShort () {
+        final var socialSecurityNumberIsValid = SocialSecurityNumberValidator.validateSocialSecurityNumber(socialSecurityNumberShort) ;
+        Assertions.assertEquals(false, socialSecurityNumberIsValid);
+    }
+
+    @Test
+    public void shouldNotValidateSocialSecurityNumberTooLong () {
+        final var socialSecurityNumberIsValid = SocialSecurityNumberValidator.validateSocialSecurityNumber(socialSecurityNumberLong) ;
+        Assertions.assertEquals(false, socialSecurityNumberIsValid);
+    }
+
+    @Test
+    public void shouldNotValidateSocialSecurityNumberWithLetter () {
+        final var socialSecurityNumberIsValid = SocialSecurityNumberValidator.validateSocialSecurityNumber(socialSecurityNumberWithLetter) ;
+        Assertions.assertEquals(false, socialSecurityNumberIsValid);
+    }
+
+    @Test
+    public void shouldValidateSocialSecurityNumber () {
+        final var socialSecurityNumberIsValid = SocialSecurityNumberValidator.validateSocialSecurityNumber(validSocialSecurityNumber) ;
+        Assertions.assertEquals(true, socialSecurityNumberIsValid);
+    }
+}
